fd2f4ab5c0073b1bd704e17be1c92f8ddb9778b6
Mechatroniker94 <danny_elito94@outlook.com>
2021-02-11 16:16:47 -0600

    i2C add

---
+++ BMP180/PIC16F887/BMP180.X/I2C.h
@@ -0,0 +1,8 @@
+void i2c_init(void);
+void i2c_start(void);
+void i2c_restart(void);
+void i2c_stop(void);
+unsigned char i2c_read(void);
+unsigned char i2c_write(unsigned char dato);
+void i2c_idle(void);
+void i2c_sendNACK(void);
---
+++ BMP180/PIC16F887/BMP180.X/build/default/production/BMP180.p1.d
@@ -0,0 +1,5 @@
+build/default/production/BMP180.p1:  \
+BMP180.c  \
+BMP180.h  \
+configuracion.h  \
+I2C.h 
